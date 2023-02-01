c292f369a87e9d2654b05a37bad419b8c8215827
pessoamarcio <pessoa-marcio@hotmail.com>
2023-02-01 11:24:26 -0300

    blog pessoal

---
+++ src/main/java/com/example/blogpessoalitau/repository/PostagemRepository.java
@@ -0,0 +1,12 @@
+package com.example.blogpessoalitau.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.blogpessoalitau.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+
+	
+}
