59e57a0c71fc658202445f48baa92538d3af519d
pessoamarcio <pessoa-marcio@hotmail.com>
2023-02-01 12:08:34 -0300

    commit

--- src/main/java/com/example/blogpessoalitau/controller/PostagemController.java
+++ src/main/java/com/example/blogpessoalitau/controller/PostagemController.java
@@ -1,23 +1,29 @@
 package com.example.blogpessoalitau.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.blogpessoalitau.model.Postagem;
 import com.example.blogpessoalitau.repository.PostagemRepository;
+
 
 @RestController
 @RequestMapping("/postagens")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
 
+	@Autowired
 	private PostagemRepository postagemRepository;
 
-@GetMapping
-public ResponseEntity<List<Postagem>> getAll{
-	return ResponseEntity.ok(postagemRepository.findAll());
-}
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
 
 }
--- src/main/java/com/example/blogpessoalitau/repository/PostagemRepository.java
+++ src/main/java/com/example/blogpessoalitau/repository/PostagemRepository.java
@@ -1,12 +1,14 @@
 package com.example.blogpessoalitau.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.blogpessoalitau.model.Postagem;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
+	//public List<Postagem> findAllbyTituloContainingIngnoreCase(String titulo);
 	
 }
